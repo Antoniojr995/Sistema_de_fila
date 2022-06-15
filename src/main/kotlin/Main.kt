15853928eadb320ece9f2979d4c8cdbee98326ff
Dantesys <ddlnob810@gmail.com>
2022-06-15 11:33:16 -0300

    melhoria na tela de listagem de medicos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,5 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
+// Copyright 2000-2021 J etBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,54 +7,74 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import java.io.File
 import Medico
+import Medicos
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.*
 import com.google.gson.Gson
+import java.io.BufferedReader
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalUnitApi::class)
 @Composable
 @Preview
-fun App() {
+fun App(medicos:Medicos) {
     var count = remember { mutableStateOf(0) }
     MaterialTheme {
-        Box(Modifier.fillMaxSize()) {
-            Button(modifier = Modifier.align(Alignment.Center),
-                onClick = {
-                    count.value++
-                }) {
-                Text(if (count.value == 0) "Hello World" else "Clicked ${count.value}!")
+        LazyVerticalGrid(cells = GridCells.Adaptive(100.dp), contentPadding = PaddingValues(5.dp), horizontalArrangement = Arrangement.spacedBy(5.dp), verticalArrangement = Arrangement.spacedBy(5.dp)){
+            items(medicos.Medicos!!.size){ medico ->
+                Card(modifier = Modifier.width(100.dp).height(100.dp),shape = RoundedCornerShape(20.dp), backgroundColor = medicos.Medicos[medico].Cor){
+                    Column(Modifier.padding(0.dp,10.dp,0.dp,0.dp)){
+                        Text(medicos.Medicos[medico].Nome,Modifier.align(Alignment.CenterHorizontally),textAlign = TextAlign.Center)
+                        Text(medicos.Medicos[medico].Setor,Modifier.align(Alignment.CenterHorizontally),textAlign = TextAlign.Center)
+                    }
+                }
             }
-            Button(modifier = Modifier.align(Alignment.Center),
-                onClick = {
-                    count.value = 0
-                }) {
-                Text("Reset")
+            item{
+                Button(onClick = {
+                    count.value++
+                },modifier = Modifier.width(100.dp).height(100.dp),shape = RoundedCornerShape(20.dp), colors = ButtonDefaults.buttonColors(Color.Green)) {
+                    Text("+",textAlign = TextAlign.Center, fontSize = 30.sp)
+                }
             }
         }
     }
 }
 fun main() = application {
     val gson:Gson = Gson()
-    val medic_list = mutableListOf(Medico("","","",arrayOf("")))
+    var medic_list:Medicos = Medicos(arrayListOf(Medico("Douglas","Pediatria", Color.Yellow,null)))
     val caminho = "./medicos.json"
     val arquivo = File(caminho)
     val isNewFileCreated :Boolean = arquivo.createNewFile()
     //VERIFICA SE O ARQUIVO FOI CRIADO
     if(isNewFileCreated){
+        var jsonString:String = gson.toJson(medic_list)
+        arquivo.writeText(jsonString)
         println("$caminho is created successfully.")
     } else{
         //ARQUIVO JÁ EXISTE
         //CARREGAR AS INFORMÇÕES
-        println("$caminho already exists.")
+        var bufferedReader: BufferedReader = arquivo.bufferedReader()
+        var inputString = bufferedReader.use { it.readText() }
+        var post = gson.fromJson(inputString, Medicos::class.java)
+        medic_list = Medicos(post.Medicos)
     }
     Window(
         onCloseRequest = ::exitApplication,
         title = "Teste",
         state = rememberWindowState(width = 800.dp, height = 600.dp)
     ) {
-        App()
+        App(medic_list)
     }
 }
+
--- src/main/kotlin/Medico.kt
+++ src/main/kotlin/Medico.kt
@@ -1,6 +1,7 @@
+import androidx.compose.ui.graphics.Color
 import java.io.Serializable
 
-data class Medico(val Nome:String, val Setor:String, val Cor:String, val Atendimentos:Array<String>?) {
+data class Medico(val Nome:String, val Setor:String, val Cor: Color, val Atendimentos:ArrayList<String>?) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -10,10 +11,7 @@ data class Medico(val Nome:String, val Setor:String, val Cor:String, val Atendim
         if (Nome != other.Nome) return false
         if (Setor != other.Setor) return false
         if (Cor != other.Cor) return false
-        if (Atendimentos != null) {
-            if (other.Atendimentos == null) return false
-            if (!Atendimentos.contentEquals(other.Atendimentos)) return false
-        } else if (other.Atendimentos != null) return false
+        if (Atendimentos != other.Atendimentos) return false
 
         return true
     }
@@ -22,10 +20,7 @@ data class Medico(val Nome:String, val Setor:String, val Cor:String, val Atendim
         var result = Nome.hashCode()
         result = 31 * result + Setor.hashCode()
         result = 31 * result + Cor.hashCode()
-        result = 31 * result + (Atendimentos?.contentHashCode() ?: 0)
+        result = 31 * result + (Atendimentos?.hashCode() ?: 0)
         return result
-    }
-    fun add(pac:String){
-        Atendimentos?.set(Atendimentos?.size,pac)
     }
 }
